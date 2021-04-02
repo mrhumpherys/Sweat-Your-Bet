db0be3291b932ba0fefc50828bce827b1d1f1b02
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-04-02 17:24:00 -0400

    made models Game and User; added bcrypt to dependencies

--- models/Game.js
+++ models/Game.js
@@ -1,6 +1,7 @@
 /*
+
 CREATE TABLE Game (
-	id INTEGER AUTO_INCREMENT,
+	id INTEGER NOT_NULL,
     time INTEGER,
     status VARCHAR(30),
     team1_id INTEGER,
@@ -12,3 +13,61 @@ CREATE TABLE Game (
 );
 */
 //! TODO
+
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Game extends Model {}
+
+Game.init(
+  {
+    GameID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    DateTime: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    HomeTeamID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "team",
+        key: "TeamID",
+      },
+    },
+    AwayTeamID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "team",
+        key: "TeamID",
+      },
+    },
+    AwayTeamScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    HomeTeamScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    HomeTeamWin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'game'
+  }
+);
+
+module.exports = Game;
