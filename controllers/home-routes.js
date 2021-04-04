c2bc862e194cfebfa2327caa81a5bfc0c2818cf7
iiTONELOC <anthonytropeano@me.com>
2021-04-03 21:29:57 -0400

    added homeroutes, data not displaying

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,25 +1,73 @@
 const router = require('express').Router();
 const { Bet, Game, User } = require('../models');
 const NBA = require('../db/_data/nba');
+const fetch = require('node-fetch');
 
 
 
 router.get('/', (req, res) => {
-     new NBA().getNews()
-    Game.findAll({})
-        .then(dbGamedata => {
-            if (!dbGamedata) {
-                res.status(404).json({ message: 'No Games on this date' });
-                return;
+    // ALSO NEED TO GET USER STATS TO RENDER FOR DASHBOARD VIEW
+    // WILL NEED TO CONVERT TO ASYNC WHEN GRABBING BOTH DATA SETS
+    // COPIED FROM DASHBOARD ROUTE
+    // ==================================================================
+    async function getNews(){
+        date = '2021-APR-03'
+        let response = await  
+        fetch(`https://fly.sportsdata.io/v3/nba/scores/json/NewsByDate/${date}`, {
+            method: 'GET',
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env.KEY
             }
-            res.render('homepage', {
-                // games
-            });
         })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        return response
+    }
+    async function getGames(){
+        date = '2021-APR-03'
+        let response = await  
+        fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
+            method: 'GET',
+            headers: {
+                'Ocp-Apim-Subscription-Key': process.env.KEY
+            }
+        })
+        return response
+    }
+    getGames()
+    .then(res=> res.json())
+    .then(data =>{
+        const games = JSON.stringify(data)
+        // console.log(games)
+        getNews()
+        .then(res=> res.json())
+        .then(newsData =>{
+            const news = JSON.stringify(newsData)
+            console.log({games:games})
+            console.log(`\n${news}`)
+            
+            // res.render('homepage',{
+            //     news, games 
+
+            // })
+        })
+    })
+    .catch(err => console.log(err))
+    // =================================================================================
+
+    // date = '2021-APR-03'
+    // fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Ocp-Apim-Subscription-Key': process.env.KEY
+    //     }
+    // })
+    //     .then(res => res.json())
+    //     .then(games => {
+    //         console.log('GAME DATA I WANT:', games)
+    //         res.render('dashboard', {
+    //             games
+    //         })
+    //     });
+    
 
 });
 
