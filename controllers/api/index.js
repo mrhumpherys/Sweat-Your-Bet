096feab8d27d9f2325bdf0b9ac844b1116ecc0dc
Kurt Pessa <kpessa@gmail.com>
2021-04-05 17:16:14 -0400

    added seed and reset api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,11 +5,13 @@ const betRoutes = require('./bet-routes');
 const gameRoutes = require('./game-routes');
 const refreshRoutes = require('./refresh-routes');
 const seedRoutes = require('./seed-routes');
+const resetRoutes = require('./reset-routes');
 
 router.use('/bets', betRoutes);
 router.use('/users', userRoutes);
 router.use('/games', gameRoutes);
 router.use('/refresh', refreshRoutes);
 router.use('/seed', seedRoutes);
+router.use('/reset', resetRoutes);
 
 module.exports = router;
