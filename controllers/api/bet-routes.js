bc1af2fb796123271da7e2e396d9bad31f85b347
iiTONELOC <anthonytropeano@me.com>
2021-04-05 20:23:19 -0400

    weird change

--- controllers/api/bet-routes.js
+++ controllers/api/bet-routes.js
@@ -12,29 +12,26 @@ router.post('/', asyncHandler(async (req, res) => {
 
   let bet = await Bet.create({ host_id, wager, game_id, pick_team_id });
 
-<<<<<<< HEAD
   host_id = req.session.user_id;
   wager = req.body.wager;
   game_id = req.body.game_id;
   pickTeamID = req.body.pickTeamID;
- Bet.create({
+  Bet.create({
     host_id,
     wager,
     game_id,
     pickTeamID,
   }).
-  then(dbBetData => res.json(dbBetData))
-  .catch(err => {
+    then(dbBetData => res.json(dbBetData))
+    .catch(err => {
       console.log(err);
       res.status(400).json(err);
-  });
-=======
+    });
   let user = await User.findByPk(host_id);
   //* WHETHER OR NOT TO DECREMENT, ALERT USER IF INSUFFICIENT FUNDS
   user.decrement('balance', { by: wager });
   res.json(bet);
 }));
->>>>>>> develop
 
 //! ----------------------------------------
 //! |      (R)EAD:  ALL BETS               |
@@ -48,8 +45,8 @@ router.get('/', asyncHandler(async (req, res) => {
 //! |      (R)EAD: BET BY ID               |
 //! ----------------------------------------
 // prettier-ignore
-router.get('/:id', asyncHandler(async(req, res) => {
-  res.json( await Bet.findByPk(req.params.id, { include: Game }));
+router.get('/:id', asyncHandler(async (req, res) => {
+  res.json(await Bet.findByPk(req.params.id, { include: Game }));
 }));
 
 //! ----------------------------------------
@@ -57,7 +54,7 @@ router.get('/:id', asyncHandler(async(req, res) => {
 //! ----------------------------------------
 //? PUT REQUEST TO ACCEPT A BET
 // prettier-ignore
-router.put('/:id', asyncHandler(async(req, res) => {
+router.put('/:id', asyncHandler(async (req, res) => {
   // let challenger_id = req.session.user_id;
   // req.body = challenger
   let bet = await Bet.update({ challenger_id: req.session.user_id ? req.session.user_id : req.body.user_id }, { where: { id: req.params.id } });
@@ -71,9 +68,9 @@ router.put('/:id', asyncHandler(async(req, res) => {
 //! |      (D)ELETE: BET BY ID             |
 //! ----------------------------------------
 // prettier-ignore
-router.delete('/:id', asyncHandler(async(req, res) => {
+router.delete('/:id', asyncHandler(async (req, res) => {
   await Bet.destroy({ where: { id: req.params.id } });
-  res.json( { message: `Bet with id: ${req.params.id} deleted successfully`});
+  res.json({ message: `Bet with id: ${req.params.id} deleted successfully` });
 }));
 
 module.exports = router;
