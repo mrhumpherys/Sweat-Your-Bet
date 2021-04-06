55cd7906c53b53ae493948633763e6cf124122f4
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-04-06 16:03:10 -0400

    made login and sign up pages and linked to sidebar navigation; made a separate stylesheets and linked dynamically to main.handlebars,  added bootstrap 5.0 and included font awesome

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,130 +1,150 @@
-const router = require('express').Router();
-const { Bet, Game, User } = require('../models');
-const fetch = require('node-fetch');
-const moment = require('moment');
+const router = require("express").Router();
+const { Bet, Game, User } = require("../models");
+const fetch = require("node-fetch");
+const moment = require("moment");
 
-
-router.get('/', (req, res) => {
-    let logged = req.session.loggedIn
-    if (!logged) {
-        async function getNews() {
-            let response = await
-                fetch(`https://fly.sportsdata.io/v3/nba/scores/json/News`, {
-                    method: 'GET',
-                    headers: {
-                        'Ocp-Apim-Subscription-Key': process.env.KEY
-                    }
-                })
-            return response
+router.get("/", (req, res) => {
+  let logged = req.session.loggedIn;
+  if (!logged) {
+    async function getNews() {
+      let response = await fetch(
+        `https://fly.sportsdata.io/v3/nba/scores/json/News`,
+        {
+          method: "GET",
+          headers: {
+            "Ocp-Apim-Subscription-Key": process.env.KEY,
+          },
         }
-        async function getGames() {
-            date = (moment(new Date()).format("YYYY-MM-DD"));
-            let response = await
-                fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
-                    method: 'GET',
-                    headers: {
-                        'Ocp-Apim-Subscription-Key': process.env.KEY
-                    }
-                })
-            return response
-        }
-        getGames()
-            .then(res => res.json())
-            .then(data => {
-                const data1 = JSON.stringify(data)
-                getNews()
-                    .then(res => res.json())
-                    .then(newsData => {
-                        const data2 = JSON.stringify(newsData)
-                        const games = JSON.parse(data1)
-                        const news = JSON.parse(data2)
-                        res.render('homepage', {
-                            news, games, loggedIn: req.session.loggedIn,
-                        })
-                    })
-            })
-            .catch(err => console.log(err))
-    }else{
-        async function getNews() {
-            let response = await
-                fetch(`https://fly.sportsdata.io/v3/nba/scores/json/News`, {
-                    method: 'GET',
-                    headers: {
-                        'Ocp-Apim-Subscription-Key': process.env.KEY
-                    }
-                })
-            return response
-        }
-        async function getGames() {
-            date = (moment(new Date()).format("YYYY-MM-DD"));
-            let response = await
-                fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
-                    method: 'GET',
-                    headers: {
-                        'Ocp-Apim-Subscription-Key': process.env.KEY
-                    }
-                })
-            return response
-        }
-        getGames()
-            .then(res => res.json())
-            .then(data => {
-                const data1 = JSON.stringify(data)
-                getNews()
-                    .then(res => res.json())
-                    .then(newsData => {
-                        const data2 = JSON.stringify(newsData)
-                        const games = JSON.parse(data1)
-                        const news = JSON.parse(data2)
-                        res.render('homepage', {
-                            news, games, loggedIn: true,
-                        })
-                    })
-            })
-            .catch(err => console.log(err))
+      );
+      return response;
     }
-
-});
-
-router.get('/bet/:id', (req, res) => {
-    // date = '2021-APR-04'
-    // let id = req.params.id
-    // fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
-    //     method: 'GET',
-    //     headers: {
-    //         'Ocp-Apim-Subscription-Key': process.env.KEY
-    //     }
-    // })
-    //     .then(res => res.json())
-    //     .then(games => {
-    //         //let game = JSON.stringify(games)
-    //         //let game1 = JSON.parse(game)
-    //         let singleGame = JSON.stringify(games.filter(data => data.GameID === ('16227')))
-    //         console.log(singleGame)
-    //         let game = JSON.parse(singleGame)
-    //         console.log(game)
-    //         res.render('bet', {
-    //             game
-    //         })
-    //     });
-
-    date = '2021-APR-04'
-    fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
-        method: 'GET',
-        headers: {
-            'Ocp-Apim-Subscription-Key': process.env.KEY
+    async function getGames() {
+      date = moment(new Date()).format("YYYY-MM-DD");
+      let response = await fetch(
+        `https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`,
+        {
+          method: "GET",
+          headers: {
+            "Ocp-Apim-Subscription-Key": process.env.KEY,
+          },
         }
-    })
-        .then(res => res.json())
-        .then(games => {
-
-            res.render(`bet/${req.params.id}`, {
-                games
-            })
-        });
-
-
+      );
+      return response;
+    }
+    getGames()
+      .then((res) => res.json())
+      .then((data) => {
+        const data1 = JSON.stringify(data);
+        getNews()
+          .then((res) => res.json())
+          .then((newsData) => {
+            const data2 = JSON.stringify(newsData);
+            const games = JSON.parse(data1);
+            const news = JSON.parse(data2);
+            res.render("homepage", {
+              style: "style.css",
+              news,
+              games,
+              loggedIn: req.session.loggedIn,
+            });
+          });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    async function getNews() {
+      let response = await fetch(
+        `https://fly.sportsdata.io/v3/nba/scores/json/News`,
+        {
+          method: "GET",
+          headers: {
+            "Ocp-Apim-Subscription-Key": process.env.KEY,
+          },
+        }
+      );
+      return response;
+    }
+    async function getGames() {
+      date = moment(new Date()).format("YYYY-MM-DD");
+      let response = await fetch(
+        `https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`,
+        {
+          method: "GET",
+          headers: {
+            "Ocp-Apim-Subscription-Key": process.env.KEY,
+          },
+        }
+      );
+      return response;
+    }
+    getGames()
+      .then((res) => res.json())
+      .then((data) => {
+        const data1 = JSON.stringify(data);
+        getNews()
+          .then((res) => res.json())
+          .then((newsData) => {
+            const data2 = JSON.stringify(newsData);
+            const games = JSON.parse(data1);
+            const news = JSON.parse(data2);
+            res.render("homepage", {
+              news,
+              games,
+              loggedIn: true,
+            });
+          });
+      })
+      .catch((err) => console.log(err));
+  }
 });
 
+router.get("/bet/:id", (req, res) => {
+  // date = '2021-APR-04'
+  // let id = req.params.id
+  // fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
+  //     method: 'GET',
+  //     headers: {
+  //         'Ocp-Apim-Subscription-Key': process.env.KEY
+  //     }
+  // })
+  //     .then(res => res.json())
+  //     .then(games => {
+  //         //let game = JSON.stringify(games)
+  //         //let game1 = JSON.parse(game)
+  //         let singleGame = JSON.stringify(games.filter(data => data.GameID === ('16227')))
+  //         console.log(singleGame)
+  //         let game = JSON.parse(singleGame)
+  //         console.log(game)
+  //         res.render('bet', {
+  //             game
+  //         })
+  //     });
+
+  date = "2021-APR-04";
+  fetch(`https://fly.sportsdata.io/v3/nba/scores/json/GamesByDate/${date}`, {
+    // style: 'style.css',
+    method: "GET",
+    headers: {
+      "Ocp-Apim-Subscription-Key": process.env.KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((games) => {
+      res.render(`bet/${req.params.id}`, {
+        games,
+      });
+    });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", {
+    style: "styleLogin.css",
+  });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup", {
+    style: "styleLogin.css",
+  });
+});
 
 module.exports = router;
--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,29 +1,29 @@
-async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
+function FormHandler(event) {
+  if (event.code === "Enter") {
+    myFunction();
+    async function myFunction() {
+      const email = document.querySelector("#email-login").value.trim();
+      const password = document.querySelector("#password-login").value.trim();
+      if (email && password) {
+        const response = await fetch("/api/users/login", {
+          method: "post",
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+          document.location.replace("/");
+        } else {
+          alert(response.statusText);
+        }
       }
     }
+  } else {
+    return;
+  }
 }
 
 async function loginFormHandler(event) {
@@ -43,7 +43,7 @@ async function loginFormHandler(event) {
         });
 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/');
             console.log('success')
         } else {
             alert(response.statusText);
@@ -51,8 +51,4 @@ async function loginFormHandler(event) {
     }
 }
 
-
-
-
-document.querySelector('#signup').addEventListener('click', signupFormHandler);
-document.querySelector('#login').addEventListener('click', loginFormHandler);
+document.querySelector('#submit').addEventListener('click', loginFormHandler);
